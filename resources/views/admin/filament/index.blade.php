2e8d7bc059916a4a550cfff448a692e29182b734
Querubin, Mosiah <que14003@byui.edu>
2017-07-27 12:24:06 -0600

    comments

--- resources/views/admin/filament/index.blade.php
+++ resources/views/admin/filament/index.blade.php
@@ -1,1 +1,8 @@
-test
+{!! BootForm::open()->action('admin/department')->post() !!}
+  {!! BootForm::text('Type', 'first_name') !!}
+  {!! BootForm::text('Cost', 'last_name') !!}
+  {!! BootForm::date('Color', 'date_of_birth') !!}
+  {!! BootForm::email('Email', 'email') !!}
+  {!! BootForm::password('Password', 'password') !!}
+  {!! BootForm::submit('Submit') !!}
+{!! BootForm::close() !!} 
