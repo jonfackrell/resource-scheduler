4379f14e1078e65e52714f0c1a8d82a4b45271f5
Jon Fackrell <jon.fackrell@gmail.com>
2017-07-31 16:01:59 -0600

    Added Models, Migrations, and Seeds for: (#9)
    - Colors
     - Print Jobs
     - Patrons

--- app/Models/Filament.php
+++ app/Models/Filament.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Filament extends Model
 {
-    //
+    /**
+     * The colors that are in stock for the filament.
+     */
+    public function colors()
+    {
+        return $this->belongsToMany('App\Models\Color', 'filaments_colors', 'color', 'filament');
+    }
 }
---
+++ database/migrations/2017_07_31_203136_create_print_jobs_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePrintJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('print_jobs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('patron')->unsigned();
+            $table->integer('department')->unsigned();
+            $table->integer('filament')->unsigned();
+            $table->integer('color')->unsigned();
+            $table->string('filename')->nullable();
+            $table->integer('time')->nullable();
+            $table->integer('weight')->nullable();
+            $table->json('options')->nullable();
+            $table->string('status')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('print_jobs');
+    }
+}
