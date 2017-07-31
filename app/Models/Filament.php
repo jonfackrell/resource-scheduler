24a2e9f0184be8f72d965ea32b1fb37b67b5c6e1
Querubin, Mosiah <que14003@byui.edu>
2017-07-31 16:53:05 -0600

    no message

--- app/Models/Filament.php
+++ app/Models/Filament.php
@@ -8,4 +8,13 @@ class Filament extends Model
 {
     //
     protected $fillable = ['name', 'description', 'quantity'];
+
+    /**
+     * The colors that are in stock for the filament.
+     */
+    public function colors()
+    {
+        return $this->belongsToMany('App\Models\Color', 'filaments_colors', 'color', 'filament');
+    }
+
 }
