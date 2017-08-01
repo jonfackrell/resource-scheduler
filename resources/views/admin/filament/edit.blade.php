f82514a439cfc067d7478c98a18c2fd1ab0f3a9f
Querubin, Mosiah <que14003@byui.edu>
2017-08-01 16:36:28 -0600

    got rid of the quantity edit box on filament. got rid of create at, deleted at, updated at boxes on all the edits.blade.php

--- resources/views/admin/filament/edit.blade.php
+++ resources/views/admin/filament/edit.blade.php
@@ -6,10 +6,7 @@
 	  {!! BootForm::bind($filament) !!}
 	  {!! BootForm::text('Name', 'name') !!}
 	  {!! BootForm::text('Description', 'description') !!}
-	  {!! BootForm::text('Quantity', 'quantity') !!}
-	  {!! BootForm::text('Created at', 'created_at') !!}
-	  {!! BootForm::text('Updated at', 'updated_at') !!}
-	  {!! BootForm::text('Deleted at', 'deleted_at') !!}
+	  
 	  {!! BootForm::submit('Submit') !!}
 	  
 	{!! BootForm::close() !!} 
