a20e256f6b79922461775b7ae6d79c51c3eb100e
Querubin, Mosiah <que14003@byui.edu>
2017-09-13 18:48:52 -0600

    I fixed the status pie chart. I also cleaned up the code and renamed the variables in the controller so that it will be easy to find.

--- app/Http/Controllers/ChartsController.php
+++ app/Http/Controllers/ChartsController.php
@@ -8,6 +8,7 @@ use App\Models\PrintJob;
 use App\Models\FilamentColor;
 use App\Models\Color;
 use App\Models\Filament;
+use App\Models\Status;
 
 use App\Http\Requests;
 use Charts;
@@ -16,50 +17,21 @@ class ChartsController extends Controller
 {
     public function index()
     {
-        // $chart = Charts::multi('bar', 'material')
-        //     // Setup the chart settings
-        //     ->title("My Cool Chart")
-        //     // A dimension of 0 means it will take 100% of the space
-        //     ->dimensions(0, 400) // Width x Height
-        //     // This defines a preset of colors already done:)
-        //     ->template("material")
-        //     // You could always set them manually
-        //     // ->colors(['#2196F3', '#F44336', '#FFC107'])
-        //     // Setup the diferent datasets (this is a multi chart)
-        //     ->dataset('Element 1', [5,20,100])
-        //     ->dataset('Element 2', [15,30,80])
-        //     ->dataset('Element 3', [25,10,40])
-        //     // Setup what the values mean
-        //     ->labels(['One', 'Two', 'Three']);
-
-        // $chart3 = Charts::database(FilamentColor::whereDepartment(auth()->user()->department)->get(), 'bar', 'google')
-            
-            // ->title('Filaments')
-
-            // ->groupBy('filament')
-            // ->labels(Filament::all()->pluck('name'));
-
-    //         $chart3 = Charts::multi('bar', 'highcharts')
-    //         ->title('Filament Colors')
-    // ->colors(['#ff0000', '#00ff00', '#0000ff'])
-    // ->labels(Color::all()->pluck('name'))
-    // ->dataset('PolyLite PLA', FilamentColor::where('department',auth()->user()->department)->where('filament', 1)->pluck('quantity'))
-    // ->dataset('nGEN', FilamentColor::where('department',auth()->user()->department)->where('filament', 2)->pluck('quantity'))
-    // ->dataset('NinjaFlex', FilamentColor::where('department',auth()->user()->department)->where('filament', 3)->pluck('quantity'));
+        
 
 
-           $chart2 = Charts::database(PrintJob::where('status', 4)->where('department', auth()->user()->department)->get(), 'line', 'highcharts')
+            $printJobLineGraph = Charts::database(PrintJob::where('status', 4)->where('department', auth()->user()->department)->get(), 'line', 'highcharts')
             ->title('Prints Per Month')
             ->groupByMonth()
             ->labels(['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'])
             ->elementLabel("Prints")
             ->dimensions(0,500);
 
-            $chart = Charts::database(PrintJob::whereDepartment(auth()->user()->department)->get(), 'pie', 'highcharts')
+            $statusPieChart = Charts::database(PrintJob::whereDepartment(auth()->user()->department)->get()->sortBy->status, 'pie', 'highcharts')
             ->title('Printjobs by status')
             ->groupBy('status')
-            ->colors(['#2196F3', '#FFC107', '#F44336', '#32CD32'])
-            ->labels(['Denied','Printing Complete','Pending Print', 'Pending Approval','Printing',]);
+            ->colors(['#2196F3', '#FFC107', '#F44336', '#32CD32', '#24ddf0'])
+            ->labels(Status::all()->pluck('name'));
 
         
 
@@ -109,16 +81,16 @@ class ChartsController extends Controller
 
             
 
-    $chart3 = Charts::multi('bar', 'highcharts')
+    $filamentQuantityByColor = Charts::multi('bar', 'highcharts')
             ->title('Filament Colors')
     ->colors(Color::all()->pluck('hex_code')->all())
     ->labels(Filament::all()->pluck('name'));
 
     foreach (Color::all() as $color) {
-        $chart3 = $chart3->dataset($color->name, FilamentColor::where('department',auth()->user()->department)->where('color', $color->id)->pluck('quantity'));
+        $filamentQuantityByColor = $filamentQuantityByColor->dataset($color->name, FilamentColor::where('department',auth()->user()->department)->where('color', $color->id)->pluck('quantity'));
     } 
     
 
-        return view('admin.charts.index', ['chart3' => $chart3, 'chart' => $chart, 'chart2' => $chart2, 'filamentChart1' => $filamentChart1, 'filamentChart2' => $filamentChart2, 'filamentChart3' => $filamentChart3]);
+        return view('admin.charts.index', ['chart3' => $filamentQuantityByColor, 'chart2' => $printJobLineGraph, 'chart' => $statusPieChart, 'filamentChart1' => $filamentChart1, 'filamentChart2' => $filamentChart2, 'filamentChart3' => $filamentChart3]);
     }
 }
