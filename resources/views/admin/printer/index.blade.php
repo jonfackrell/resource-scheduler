b9232219b761efa0f2526af9e607bfb3bf12c630
Fackrell <fackrelj@byui.edu>
2017-09-26 16:42:04 -0600

    no message

--- resources/views/admin/printer/index.blade.php
+++ resources/views/admin/printer/index.blade.php
@@ -14,7 +14,7 @@
 			{!! BootForm::text('Flat Printing Fee', 'flat_fee')->helpBlock('You can add a flat printing fee to use this printer. Value should be in cents.') !!}
 			{!! BootForm::text('Per Hour Fee', 'per_hour')->helpBlock('You can charge a per hour fee to use this printer. Value should be in cents.') !!}
 			{!! BootForm::text('Overtime Fee', 'overtime_fee')->helpBlock('Alternatively, you can charge a fee for every X hours. For example, add $1 for every 12 hours of printing. Value should be in cents.') !!}
-			{!! BootForm::text('Start Charging Overtime At', 'overtime_start')->helpBlock('If charging an overtime fee, specficy the interval for when the fee should be applied. Value should be in hours.') !!}
+			{!! BootForm::text('Start Charging Overtime At', 'overtime_start')->helpBlock('If charging an overtime fee, specficy the interval for when the fee should be applied. Value should be in minutes.') !!}
 		  	{!! BootForm::submit('Submit') !!}
 		{!! BootForm::close() !!}
 	@endif
