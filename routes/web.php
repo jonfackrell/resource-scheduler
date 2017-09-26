29fb916c1a793e265fd9d7826c8e2072be42a76b
Fackrell <fackrelj@byui.edu>
2017-09-26 15:30:53 -0600

    no message

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Auth::routes();
+Route::group(['middleware' => ['mail']], function() {
+    Auth::routes();
+});
 
 Route::group(['middleware' => ['auth', 'mail']], function() {
 
