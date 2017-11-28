f43dee77ded69220f6f9016ff15a6449e73c6ccb
Fackrell <fackrelj@byui.edu>
2017-11-28 12:32:05 -0700

    added wait times to printer page

--- resources/views/public/printers.blade.php
+++ resources/views/public/printers.blade.php
@@ -9,23 +9,32 @@
     @foreach($printers as $printer)
         <div class="row">
             <div class="col-md-3">
-                <img src="{{ $printer->image }}" alt="" style="width: 100%; height: auto; max-width: 400px;"/>
+                <p>
+                    <img src="{{ $printer->image }}" alt="" style="width: 100%; height: auto; max-width: 400px;"/>
+                </p>
+                <br />
+                <div class="tile" style="text-align: center;">
+                    <span>Estimated Start</span>
+                    <h2>{!! $printer->timeToPrint()->diffForHumans() !!}</h2>
+                </div>
             </div>
-            <div class="col-md-9">
+            <div class="col-md-6">
                 <h2>{{ $printer->name }} <small>{{ $printer->departmentOwner->name }}</small></h2>
                 {!! $printer->description !!}
                 <div class="row">
                     @foreach($printer->filaments as $filament)
-                        <div class="col-md-12" style="padding-left: 24px;">
-                            <h3><small>{{ $filament->name }}</small></h3>
-                            <p>{!! $filament->description !!}</p>
-                            <div>
-                                <div style="font-weight: bolder; display: inline-block; vertical-align: top; padding-top: 1px; padding-left: 18px;">Filament Colors: </div>
-                                @foreach($filament->colors($printer->departmentOwner->id) as $color)
-                                    <div style="height: 20px; width: 20px; display: inline-block; background-color: #{{ $color->hex_code }}" title="{{ $color->name }}"></div>
-                                @endforeach
+                        @if($filament->colors($printer->departmentOwner->id)->count() > 0)
+                            <div class="col-md-12" style="padding-left: 24px;">
+                                <h3><small>{{ $filament->name }}</small></h3>
+                                <p>{!! $filament->description !!}</p>
+                                <div>
+                                    <div style="font-weight: bolder; display: inline-block; vertical-align: top; padding-top: 1px; padding-left: 18px;">Filament Colors: </div>
+                                    @foreach($filament->colors($printer->departmentOwner->id) as $color)
+                                        <div style="height: 20px; width: 20px; display: inline-block; background-color: #{{ $color->hex_code }}" title="{{ $color->name }}"></div>
+                                    @endforeach
+                                </div>
                             </div>
-                        </div>
+                        @endif
                     @endforeach
                 </div>
             </div>
