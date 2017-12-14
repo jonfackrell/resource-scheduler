595e8e540b0cd21ae09351721d81cb904978fdff
Fackrell <fackrelj@byui.edu>
2017-12-14 15:05:31 -0700

    no message

--- resources/views/admin/settings/index.blade.php
+++ resources/views/admin/settings/index.blade.php
@@ -10,9 +10,9 @@
         {!! BootForm::open()->action(route('settings.index'))->post() !!}
             {!! BootForm::text('Logo', 'LOGO')->defaultValue(((isset($settings->where('name', 'LOGO')->first()->value))?$settings->where('name', 'LOGO')->first()->value:'')) !!}
             {!! BootForm::textarea('Header HTML', 'HEADER_HTML')->addClass('summernote')->defaultValue(((isset($settings->where('name', 'HEADER_HTML')->first()->value))?$settings->where('name', 'HEADER_HTML')->first()->value:'')) !!}
-            {!! BootForm::textarea('Custom CSS', 'HEADER_CSS')->addClass('summernote')->defaultValue(((isset($settings->where('name', 'HEADER_CSS')->first()->value))?$settings->where('name', 'HEADER_CSS')->first()->value:'')) !!}
+            {!! BootForm::textarea('Custom CSS', 'HEADER_CSS')->defaultValue(((isset($settings->where('name', 'HEADER_CSS')->first()->value))?$settings->where('name', 'HEADER_CSS')->first()->value:'')) !!}
             {!! BootForm::textarea('Footer HTML', 'FOOTER_HTML')->addClass('summernote')->defaultValue(((isset($settings->where('name', 'FOOTER_HTML')->first()->value))?$settings->where('name', 'FOOTER_HTML')->first()->value:'')) !!}
-            {!! BootForm::textarea('Custom JavaScript', 'FOOTER_JS')->addClass('summernote')->defaultValue(((isset($settings->where('name', 'FOOTER_JS')->first()->value))?$settings->where('name', 'FOOTER_JS')->first()->value:'')) !!}
+            {!! BootForm::textarea('Custom JavaScript', 'FOOTER_JS')->defaultValue(((isset($settings->where('name', 'FOOTER_JS')->first()->value))?$settings->where('name', 'FOOTER_JS')->first()->value:'')) !!}
 
             @php
                 if(!is_null($email)){
