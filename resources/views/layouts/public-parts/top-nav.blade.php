84de8a1a355e97cdc0da70987c11886666cb1d88
Jon Fackrell <jon.fackrell@gmail.com>
2018-09-14 07:54:00 -0600

    no message

--- resources/views/layouts/public-parts/top-nav.blade.php
+++ resources/views/layouts/public-parts/top-nav.blade.php
@@ -12,30 +12,8 @@
             </a>
 
         </div>
-<<<<<<< HEAD
-        <ul class="nav navbar-nav">
-            <li>
-                <a href="{{ route('printers') }}" class="navbar-link">3D Printers</a>
-            </li>
-            <li>
-                <a href="{{ route('options') }}" class="navbar-link">Upload</a>
-            </li>
-            <li>
-                <a href="{{ route('history') }}" class="navbar-link">View History</a>
-            </li>
-            <li>
-                <a href="{{ route('policy') }}" class="navbar-link">Policy</a>
-            </li>
-            <li>
-                <a href="{{ route('contact') }}" class="navbar-link">Contact Us</a>
-            </li>
-        </ul>
-        @if(auth()->guard('patrons')->check())
-            <ul class="nav navbar-nav navbar-right">
-=======
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
->>>>>>> ac2c7cec6acde95db9b54c4ce81d06f0e9f7247c
                 <li>
                     <a href="{{ route('printers') }}" class="navbar-link">3D Printers</a>
                 </li>
